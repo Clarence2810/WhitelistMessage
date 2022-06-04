afbec2d92801cf47a34961a488d5fb81fb8ce949
Kuu <69802296+Kuuuuuuuu@users.noreply.github.com>
2022-06-04 17:21:02 +0700

    Update PMMP 4.0

--- src/Clarence2810/WhitelistMessage/Main.php
+++ src/Clarence2810/WhitelistMessage/Main.php
@@ -2,26 +2,29 @@
 
 namespace Clarence2810\WhitelistMessage;
 
-use pocketmine\{
-	Player,
-	event\Listener,
-	event\player\PlayerPreLoginEvent,
-	plugin\PluginBase,
-	utils\Textformat as C,
-};
+use pocketmine\{event\Listener, event\player\PlayerLoginEvent, plugin\PluginBase, Server, utils\Textformat as C};
+
 class Main extends PluginBase implements Listener
 {
-	public function onEnable(){
-		$this->saveDefaultConfig();
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
-	
-	public function onPreLogin(PlayerPreLoginEvent $event):void{
-		$player = $event->getPlayer();
-		if(!$player->isWhitelisted()){
-			if($this->getConfig()->get("whitelist-notify") === true) foreach($this->getServer()->getOnlinePlayers() as $players) if($players->hasPermission("whitelistmessage.alert") || $players->isWhitelisted()) $players->sendMessage(str_replace(["{player}", "&"], [$player->getName(), C::ESCAPE], $this->getConfig()->get("whitelist-notify-message")));
-			$event->setKickMessage(str_replace("&", C::ESCAPE, $this->getConfig()->get("kick-message")));
-			$event->setCancelled();
-		}
-	}
+    public function onEnable(): void
+    {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPreLogin(PlayerLoginEvent $event): void
+    {
+        $player = $event->getPlayer();
+        if (!Server::getInstance()->isWhitelisted($player->getName())) {
+            if ($this->getConfig()->get("whitelist-notify") === true) {
+                foreach ($this->getServer()->getOnlinePlayers() as $players) {
+                    if ($players->hasPermission("whitelistmessage.alert") || Server::getInstance()->isWhitelisted($players->getName())) {
+                        $players->sendMessage(str_replace(["{player}", "&"], [$player->getName(), C::ESCAPE], $this->getConfig()->get("whitelist-notify-message")));
+                    }
+                }
+            }
+            $event->setKickMessage(str_replace("&", C::ESCAPE, $this->getConfig()->get("kick-message")));
+            $event->cancel();
+        }
+    }
 }
